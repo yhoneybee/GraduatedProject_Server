c267ba84a4064019645640e68d530f82f6595ffd
yhoneybee <chaeyh04@gmail.com>
2022-07-04 01:29:14 +0900

    < update > connect ip localhost

--- GraduatedProject_Server/GraduatedProject_Server/MySQL.cs
+++ GraduatedProject_Server/GraduatedProject_Server/MySQL.cs
@@ -71,7 +71,7 @@ namespace GraduatedProject_Server
     public class MySQL : ISQL
     {
         //readonly string server = "Server=119.196.245.41;";
-        readonly string server = "Server=3.36.125.83;";
+        readonly string server = "Server=127.0.0.1;";
         readonly string port = "Port=3306;";
         readonly string db = "Database=db;";
         readonly string id = "Uid=root;";
